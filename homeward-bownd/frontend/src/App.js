428fa623b451749e9e8ea2476cc3d8e462965d98
John Allan Hinds <jabistro@gmail.com>
2025-02-11 20:01:56 -0500

    App.js update

--- homeward-bownd/frontend/src/App.js
+++ homeward-bownd/frontend/src/App.js
@@ -2,16 +2,19 @@ import Home from "./components/Home/Home";
 import Login from "./components/Login/Login";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Search from "./components/Search/Search";
+import { AuthProvider } from './context/AuthContext';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/search" element={<Search />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/search" element={<Search />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
